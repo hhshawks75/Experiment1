73e03d1b2534c793032da177c8542c06123e9ece
Zachary Almaraz <47463264+hhshawks75@users.noreply.github.com>
2021-07-22 00:15:22 -0500

    Delete package-info.java

--- package-info.java
+++
@@ -1,7 +0,0 @@
-/** This package contains classes and methods for sentiment analysis
-    CS 3354 Spring 2019, Texas State University.
-    @author metsis
-    @author tesic
-    @author wen
-*/
-package assign5;
